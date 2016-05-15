fea5e6f70d9a17f7898025eede137b62ff0c9425
Ming HU <humings@gmail.com>
2016-05-15 23:17:37 +0800

    UI for login page is done

--- on_the_map/LoginViewController.swift
+++ on_the_map/LoginViewController.swift
@@ -8,28 +8,22 @@
 
 import UIKit
 import Alamofire
+import Reachability
 
+class LoginViewController: UIViewController, UITextFieldDelegate {
+    
 
-class LoginViewController: UIViewController {
+    @IBOutlet weak var emailTextField: UITextField!
 
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    var email: String?
+    var password: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        let parameters = [
-            "udacity": [
-                "username": "humings@gmail.com",
-                "password": "bertrandoosc0$"
-            ]
-        ]
-        
-        Alamofire.request(.POST, "https://www.udacity.com/api/session", parameters: parameters, encoding: .JSON)
-            .responseString { response in
-                print(response.data)
-                print(response.result)
-                print(response.result.value)
-        }
-        
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
         
     }
 
@@ -37,7 +31,86 @@ class LoginViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // UITextFieldDelegate
+    func textFieldDidBeginEditing(textField: UITextField) {
+        textField.text = ""
+    }
+    
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        if textField == emailTextField {
+            email = textField.text
+        } else if textField == passwordTextField {
+            password = textField.text
+        }
+        return true
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        loginToUdacity()
+        return true
+    }
 
 
+    @IBAction func pressLoginButton(sender: AnyObject) {
+        self.becomeFirstResponder()
+        loginToUdacity()
+    }
+    
+    func loginToUdacity() {
+        print(email)
+        print(password)
+        var errorMessage: String?
+        
+        let reachability: Reachability
+        do {
+            reachability = try Reachability.reachabilityForInternetConnection()
+        } catch {
+            print("Unable to create Reachability")
+            return
+        }
+
+        reachability.whenUnreachable = { reachability in
+            // this is called on a background thread, but UI updates must
+            // be on the main thread, like this:
+            dispatch_async(dispatch_get_main_queue()) {
+                self.displayErrorMessage("Network not reachable")
+            }
+        }
+        
+        
+        if email == nil || email == "" || password == nil || password == "" {
+            errorMessage = "Email or Password is empty"
+        }
+        
+        if let errorMessage = errorMessage {
+            displayErrorMessage(errorMessage)
+        }
+
+        // login here
+        let parameters = [
+            "udacity": [
+                "username": email!,
+                "password": password!
+            ]
+        ]
+
+        Alamofire.request(.POST, "https://www.udacity.com/api/session", parameters: parameters, encoding: .JSON)
+            .responseString { response in
+                print(response.data)
+                print(response.result)
+                print(response.result.value)
+        }
+        
+
+    }
+    
+    func displayErrorMessage(message: String) {
+        let alert = UIAlertController(title: "", message: message, preferredStyle: .Alert)
+        let defaultAction = UIAlertAction(title: "OK", style: .Default, handler: nil)
+        alert.addAction(defaultAction)
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
 }
 
