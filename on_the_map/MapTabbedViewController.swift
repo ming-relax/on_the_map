0a9bd4ee1cf3af59b3a498121817a0567587d818
Ming HU <humings@gmail.com>
2016-05-23 23:25:46 +0800

    Add map view

---
+++ on_the_map/MapTabbedViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MapTabbedViewController.swift
+//  on_the_map
+//
+//  Created by Ming Hu on 5/23/16.
+//  Copyright © 2016 Ming Hu. All rights reserved.
+//
+
+import Foundation
+import MapKit
+import UIKit
+
+class MapTabbedViewController: UIViewController {
+    
+    @IBOutlet weak var map: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
